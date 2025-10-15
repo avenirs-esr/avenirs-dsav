0097f51e92aafc554617d73dd8a2d29341a2b44c
Pascal Béger <pascal.beger@gmail.com>
2025-10-15 08:54:27 +0200

    feat: add useBreakpoint composable

---
+++ src/config/breakpoints.ts
@@ -0,0 +1,10 @@
+/**
+ * Breakpoints as defined in src/styles/breakpoints.scss
+ * Values are in px to be able to work with window.with
+ */
+export enum BREAKPOINTS {
+  SM = 576, /* 36rem */
+  MD = 768, /* 48rem */
+  LG = 1024, /* 64rem */
+  XL = 1440, /* 90rem */
+}
