8d3aafb85051e49e5b9c96abf5e1b889e9322001
Pascal Béger <pascal.beger@gmail.com>
2025-10-17 09:41:32 +0200

    build(695): export DSFR style in production bundle

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,11 +1,5 @@
 import { type Preview, setup } from '@storybook/vue3-vite'
 
-import '@gouvfr/dsfr/dist/core/core.main.min.css'
-import '@gouvfr/dsfr/dist/component/component.main.min.css'
-import '@gouvfr/dsfr/dist/utility/utility.main.min.css'
-import '@gouvminint/vue-dsfr/styles'
-import '@gouvfr/dsfr/dist/scheme/scheme.min.css'
-import '@gouvfr/dsfr/dist/utility/icons/icons.min.css'
 import '@/styles/main.scss'
 import './preview.scss'
 
