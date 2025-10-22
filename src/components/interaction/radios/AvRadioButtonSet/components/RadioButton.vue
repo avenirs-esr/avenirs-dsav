a73c15510a0894f73f5b1cd5aed299544f96305c
Pascal Béger <pascal.beger@gmail.com>
2025-10-22 12:44:40 +0200

    refactor(700): remove VueDSFR from AvRadioButtonSet

---
+++ src/components/interaction/radios/AvRadioButtonSet/components/RadioButton.vue
@@ -0,0 +1,101 @@
+<script lang="ts" setup>
+import type { Slot } from 'vue'
+
+/**
+ * RadioButton component props.
+ */
+export interface RadioButtonProps {
+  /**
+   * Value of the radio button.
+   * This value will be emitted when the radio is selected.
+   */
+  value: string | number | boolean
+
+  /**
+   * Model value of the radio button.
+   */
+  modelValue: string | number | boolean | undefined
+
+  /**
+   * If true, disables this radio button.
+   */
+  disabled?: boolean
+
+  /**
+   * If true, displays the button in its small version.
+   */
+  small?: boolean
+
+  /**
+   * If true, displays the button in its inline version.
+   */
+  inline?: boolean
+
+  /**
+   * Name of the input field.
+   * Must be the same for each AvRadioButton in an AvRadioButtonSet
+   * @default ''
+   */
+  name?: string
+}
+
+const {
+  value,
+  modelValue,
+  disabled,
+  small = false,
+  inline = false,
+  name = '',
+} = defineProps<RadioButtonProps>()
+
+const emit = defineEmits<{
+  /**
+   * Emitted when the selected radio button changes.
+   *
+   * @event update:modelValue
+   * @param value The newly selected value.
+   * Can be a `string`, `number`, or `boolean`, depending on the bound data.
+   */
+  (e: 'update:modelValue', value: string | number | boolean): void
+}>()
+
+defineSlots<{
+  /**
+   * Default slot used to fully customize the radio label.
+   */
+  default?: Slot
+}>()
+
+const id = ref(`av-radio-button-${crypto.randomUUID()}`)
+</script>
+
+<template>
+  <div
+    class="fr-fieldset__element"
+    :class="{ 'fr-fieldset__element--inline': inline }"
+  >
+    <div
+      class="fr-radio-group"
+      :class="{
+        'fr-radio-group--sm': small,
+      }"
+    >
+      <input
+        :id="id"
+        type="radio"
+        :name="name"
+        :value="value"
+        :checked="modelValue === value"
+        :disabled="disabled"
+        v-bind="$attrs"
+        @click="emit('update:modelValue', value)"
+      >
+      <label
+        :for="id"
+        class="fr-label"
+      >
+        <slot />
+      </label>
+    </div>
+  </div>
+</template>
