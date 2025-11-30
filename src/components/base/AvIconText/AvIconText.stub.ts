32ec32387bc802b339fa71f69cc657f3fe7f22dc
Karim EL RHOURHA <karim.elrhourha@gmail.com>
2025-11-30 15:10:01 +0100

    feat(778): update AvIconTextStub

--- src/components/base/AvIconText/AvIconText.stub.ts
+++ src/components/base/AvIconText/AvIconText.stub.ts
@@ -1,5 +1,5 @@
 export const AvIconTextStub = defineComponent({
   name: 'AvIconText',
   template: `<div class="av-icon-text" />`,
-  props: ['icon', 'text']
+  props: ['icon', 'text', 'icon-color', 'text-color']
 })
