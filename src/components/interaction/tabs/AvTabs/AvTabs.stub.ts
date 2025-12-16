56739b531017f46a3b0d2bc53f0ae59a727da0da
Pascal Béger <pascal.beger@gmail.com>
2025-12-16 10:29:58 +0100

    fix: update AvTabs stub

--- src/components/interaction/tabs/AvTabs/AvTabs.stub.ts
+++ src/components/interaction/tabs/AvTabs/AvTabs.stub.ts
@@ -1,19 +1,23 @@
+import { h } from 'vue'
+
 export const AvTabsStub = defineComponent({
   name: 'AvTabs',
   props: {
     modelValue: {
       type: Number,
-      required: false
+      default: 0,
     },
-    ariaLabel: {
-      type: String,
-      required: false
-    },
-    compact: {
-      type: Boolean,
-      required: false
-    }
+    ariaLabel: String,
+    compact: Boolean,
   },
   emits: ['update:modelValue'],
-  template: '<div class="av-tabs"><slot /></div>'
+  setup (props, { slots }) {
+    const tabItems = computed(() => slots.default?.() ?? [])
+
+    const activeTab = computed(() => props.modelValue ?? 0)
+
+    return () => {
+      return h('div', { class: 'av-tabs' }, tabItems.value[activeTab.value] ?? null)
+    }
+  },
 })
