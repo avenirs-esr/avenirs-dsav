e496d8ea9f7f544c5d84fac891c2df768db997f5
Nathan <nathan.pignon79@gmail.com>
2026-01-22 17:35:04 +0100

    fix: footer slot in av card stub

--- src/components/cards/AvCard/AvCard.stub.ts
+++ src/components/cards/AvCard/AvCard.stub.ts
@@ -2,12 +2,19 @@ export const AvCardStub = defineComponent({
   name: 'AvCard',
   props: {
     titleOnly: { type: Boolean, default: false },
-    baclgroundColor: { type: String, default: 'var(--card)' },
+    backgroundColor: { type: String, default: 'var(--card)' },
     borderColor: { type: String, default: 'var(--stroke)' },
     titleBackground: { type: String, default: 'var(--surface-background)' },
     titleHeight: { type: String },
     collapsible: { type: Boolean, default: false },
     collapsed: { type: Boolean, default: false },
   },
-  template: '<div class="av-card"><slot name="title" /><slot /></div>',
+  template: `
+    <div class="av-card">
+      <slot name="title" />
+      <slot />
+      <slot name="body" />
+      <slot name="footer" />
+    </div>
+  `,
 })
