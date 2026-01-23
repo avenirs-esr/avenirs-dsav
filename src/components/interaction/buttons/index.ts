bf844ce39d98403b11243bacea131ba9b7e2d5df
Karim EL RHOURHA <karim.elrhourha@gmail.com>
2026-01-23 17:16:56 +0100

    feat(957): export AvCancelConfirmButtonsProps

--- src/components/interaction/buttons/index.ts
+++ src/components/interaction/buttons/index.ts
@@ -1,4 +1,4 @@
 export { default as AvButton, type AvButtonProps } from './AvButton/AvButton.vue'
-export { default as AvCancelConfirmButtons } from './AvCancelConfirmButtons/AvCancelConfirmButtons.vue'
+export { default as AvCancelConfirmButtons, type AvCancelConfirmButtonsProps } from './AvCancelConfirmButtons/AvCancelConfirmButtons.vue'
 export { default as AvLanguageSelector, type AvLanguageSelectorElement, type AvLanguageSelectorProps } from './AvLanguageSelector/AvLanguageSelector.vue'
 export { default as AvRichButton } from './AvRichButton/AvRichButton.vue'
