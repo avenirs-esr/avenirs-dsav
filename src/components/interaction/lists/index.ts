7e585a8261264e1adfa5825303ff6e9ee59f10ba
Pascal Béger <pascal.beger@gmail.com>
2026-03-23 15:39:39 +0100

    fix(1214): export AvCheckboxListItem

--- src/components/interaction/lists/index.ts
+++ src/components/interaction/lists/index.ts
@@ -1,2 +1,3 @@
-export { default as AvList } from './AvList/AvList.vue'
-export { default as AvListItem } from './AvListItem/AvListItem.vue'
+export { default as AvCheckboxListItem, type AvCheckboxListItemProps } from './AvCheckboxListItem/AvCheckboxListItem.vue'
+export { default as AvList, type AvListProps } from './AvList/AvList.vue'
+export { default as AvListItem, type AvListItemProps } from './AvListItem/AvListItem.vue'
