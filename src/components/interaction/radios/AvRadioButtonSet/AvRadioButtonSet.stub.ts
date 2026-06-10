88c0b1a737f4d37d0ff4d47e816a940012a7dec3
Pascal Béger <pascal.beger@gmail.com>
2026-06-10 07:42:18 +0200

    feat(1711): AvRadioButtonSet - add errorMessage prop to stub

--- src/components/interaction/radios/AvRadioButtonSet/AvRadioButtonSet.stub.ts
+++ src/components/interaction/radios/AvRadioButtonSet/AvRadioButtonSet.stub.ts
@@ -5,6 +5,7 @@ export const AvRadioButtonSetStub = defineComponent({
     name: String,
     inline: Boolean,
     small: Boolean,
+    errorMessage: { type: String, required: false },
   },
   emits: ['update:modelValue'],
   template: '<div class="av-radio-button-set-stub" data-testid="av-radio-button-set-stub"><slot /></div>',
