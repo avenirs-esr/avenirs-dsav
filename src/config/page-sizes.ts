88ff0223531e0fde802c5f48547a07dee2b72ec9
Karim EL RHOURHA <karim.elrhourha@gmail.com>
2026-06-23 14:44:38 +0200

    feat(1778): add PageSize and css class

--- src/config/page-sizes.ts
+++ src/config/page-sizes.ts
@@ -1,11 +1,13 @@
 export enum PageSizes {
   FOUR = 4,
   EIGHT = 8,
-  TWELVE = 12
+  TWELVE = 12,
+  TWENTY = 20
 }
 
 export const pageSizeValues = [
   PageSizes.FOUR,
   PageSizes.EIGHT,
-  PageSizes.TWELVE
+  PageSizes.TWELVE,
+  PageSizes.TWENTY
 ] as const satisfies readonly PageSizes[]
